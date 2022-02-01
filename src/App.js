ec407ce81143d4afb8994d2b6faaddf00f704bb3
raymond cuya <raymondcuya@gmail.com>
2022-02-01 20:54:07 +1100

    updated with a shop page, collection preview and item component

--- src/App.js
+++ src/App.js
@@ -1,20 +1,15 @@
-import HomePage from './pages/homepage/homepage.component';
-
-import './App.css';
 import { Route, Switch } from 'react-router-dom';
+import './App.css';
 
-const HatsPage = () => (
-  <div>
-    <h1>Hats Page</h1>
-  </div>
-);
+import HomePage from './pages/homepage/homepage.component';
+import ShopPage from './pages/shop/shop.component';
 
 function App() {
   return (
     <div className="App">
       <Switch>
         <Route exact path="/" component={HomePage} />
-        <Route path="/hats" component={HatsPage} />
+        <Route path="/shop" component={ShopPage} />
       </Switch>
     </div>
   );
